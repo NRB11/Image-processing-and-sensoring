d30e6a638c26836c801e911d43b67db1e35261ad
NRB11 <34749332+NRB11@users.noreply.github.com>
2023-09-26 23:16:33 +0200

    Update MiniprojectV2.py

--- miniproject/MiniprojectV2.py
+++ miniproject/MiniprojectV2.py
@@ -1,16 +1,45 @@
 import cv2
 import numpy as np
 import struct
+import functools
 
 img = cv2.imread("miniproject/pictures/1.jpg")
 img2 = cv2.cvtColor(img,cv2.COLOR_BGR2HSV)
 
 images = { #declare dictionary with stuff (same as hashmap)
-    struct.pack('ii', 0, 0): img2[0:100, 0:100],
+    struct.pack('ii', 0, 0): img2[0:100,   0:100],
     struct.pack('ii', 1, 0): img2[0:100, 100:200],
-    struct.pack('ii', 2, 0): img2[0:100, 200:300]
-
+    struct.pack('ii', 2, 0): img2[0:100, 200:300],
+    struct.pack('ii', 3, 0): img2[0:100, 300:400],
+    struct.pack('ii', 4, 0): img2[0:100, 400:500],
+    
+    struct.pack('ii', 0, 1): img2[100:200,   0:100],
+    struct.pack('ii', 1, 1): img2[100:200, 100:200],
+    struct.pack('ii', 2, 1): img2[100:200, 200:300],
+    struct.pack('ii', 3, 1): img2[100:200, 300:400],
+    struct.pack('ii', 4, 1): img2[100:200, 400:500],
+    
+    struct.pack('ii', 0, 2): img2[200:300,   0:100],
+    struct.pack('ii', 1, 2): img2[200:300, 100:200],
+    struct.pack('ii', 2, 1): img2[200:300, 200:300],
+    struct.pack('ii', 3, 1): img2[200:300, 300:400],
+    struct.pack('ii', 4, 1): img2[200:300, 400:500],
+    
+    struct.pack('ii', 0, 3): img2[300:400,   0:100],
+    struct.pack('ii', 1, 3): img2[300:400, 100:200],
+    struct.pack('ii', 2, 3): img2[300:400, 200:300],
+    struct.pack('ii', 3, 3): img2[300:400, 300:400],
+    struct.pack('ii', 4, 3): img2[300:400, 400:500],
+    
+    struct.pack('ii', 0, 4): img2[400:500,   0:100],
+    struct.pack('ii', 1, 4): img2[400:500, 100:200],
+    struct.pack('ii', 2, 4): img2[400:500, 200:300],
+    struct.pack('ii', 3, 4): img2[400:500, 300:400],
+    struct.pack('ii', 4, 4): img2[400:500, 400:500],
 }
-print(np.average(images[struct.pack('ii', 0, 0)]))
-print(np.average(images[struct.pack('ii', 1, 0)]))
-print(np.average(images[struct.pack('ii', 2, 0)]))
+
+for x in range(5):
+    for y in range(5):
+        if struct.pack('ii', x, y) in images:
+            print(x, y, np.average(images[struct.pack('ii', x, y)]))
+
