6ee5bf27de37a11e3cdf0051f272cec796734f85
NicolajRB <34749332+NRB11@users.noreply.github.com>
2023-10-10 11:35:29 +0200

    +

---
+++ Test_Projects/lecture 9.py
@@ -0,0 +1,15 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+plt.style.use('_mpl-gallery')
+arr = np.loadtxt("Test_Projects/trainClass1.dat",converters=float)
+
+print(arr)
+
+x_axis_data = arr
+y_axis_data = arr
+
+plt.scatter(x_axis_data, y_axis_data, s=None, c=None, marker=None, cmap=None, vmin=None, vmax=None, alpha=None, linewidths=None, edgecolors=None) 
+
+plt.show()
