1f940e5cee16f90a690995882185f484177adb19
NicolajRB <34749332+NRB11@users.noreply.github.com>
2023-10-16 08:19:31 +0200

    Update lecture 9.py

--- Test_Projects/lecture 9.py
+++ Test_Projects/lecture 9.py
@@ -8,7 +8,7 @@ arr = np.loadtxt("Test_Projects/trainClass1.dat",converters=float)
 print(arr)
 
 x_axis_data = 400
-y_axis_data = len(arr)
+y_axis_data = arr
 
 plt.scatter(x_axis_data, y_axis_data, s=None, c=None, marker=None, cmap=None, vmin=None, vmax=None, alpha=None, linewidths=None, edgecolors=None) 
 
