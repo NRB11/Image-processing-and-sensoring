ee4297bd8161ee312e793084a8684c1663e92e92
NRB11 <34749332+NRB11@users.noreply.github.com>
2023-10-19 23:32:12 +0200

    Update chatcode.py

--- miniproject/chatcode.py
+++ miniproject/chatcode.py
@@ -11,8 +11,8 @@ def get_cell(x, y, res):
     cell_y = y // cell_height
     return cell_x, cell_y
 
-def print_coordinates_in_cells(contours, res, exclude_region):
-    cell_centers = []
+def print_cells_of_contours(contours, res, exclude_region):
+    cell_numbers = []
 
     for i, contour in enumerate(contours):
         # Find the center of mass of the object
@@ -21,58 +21,56 @@ def print_coordinates_in_cells(contours, res, exclude_region):
             cX = int(M["m10"] / M["m00"])
             cY = int(M["m01"] / M["m00"])
             cell_x, cell_y = get_cell(cX, cY, res)
-            
-            if 0 <= cell_x < res and 0 <= cell_y < res and not (exclude_region[0] <= cX < exclude_region[1] and exclude_region[2] <= cY < exclude_region[3]):
-                print(f"Object {i + 1} - Center Coordinates (x, y): ({cX}, {cY}), Cell: ({cell_x}, {cell_y})")
-                cell_centers.append((cell_x, cell_y))
 
-    return cell_centers
+            if 0 <= cell_x < res and 0 <= cell_y < res and not (exclude_region[0] <= cX < exclude_region[1] and exclude_region[2] <= cY < exclude_region[3]):
+                cell_number = cell_x + cell_y * res
+                cell_numbers.append(cell_number)
+
+    return cell_numbers
 
 img = cv2.imread("miniproject/pictures/1.jpg")
 img2 = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 
-res = 500
-
-coloraverages_red = np.zeros((res, res))
-coloraverages_green = np.zeros((res, res))
-coloraverages_blue = np.zeros((res, res))
-for x in range(res):
-    for y in range(res):
-        avg_color_per_row = np.average(img[0 + x * int(500/res):int(500/res) + x * int(500/res), 0 + y * int(500/res):int(500/res) + y * int(500/res)], axis=0)
-        avg_color = np.average(avg_color_per_row, axis=0)
-        coloraverages_red[x, y] = avg_color[2]
-        coloraverages_green[x, y] = avg_color[1]
-        coloraverages_blue[x, y] = avg_color[0]
-
-average_color_image = np.dstack((coloraverages_red, coloraverages_green, coloraverages_blue))
-normalized_color_averages = np.stack((coloraverages_red, coloraverages_green, coloraverages_blue), axis=-1) / 255.0
+res = 5  # 5x5 grid, 25 cells
 
 # Threshold the HSV image to isolate the yellow color
-lower_yellow = np.array([20, 100, 180])  # Lower bound for yellow color in HSV
+lower_yellow = np.array([20, 100, 175])  # Lower bound for yellow color in HSV
 upper_yellow = np.array([30, 255, 255])  # Upper bound for yellow color in HSV
 yellow_mask = cv2.inRange(img2, lower_yellow, upper_yellow)
 
 # Find contours in the yellow mask
 contours, _ = cv2.findContours(yellow_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
-# Print the number of yellow objects found
-print(f"Number of yellow objects: {len(contours)}")
-
 # Define the region (x=400:500, y=400:500) to exclude yellow detection
 exclude_region = (400, 500, 400, 500)
 
-# Get cell positions for contours while excluding the specified region
-cell_centers = print_coordinates_in_cells(contours, res, exclude_region)
+# Get cell numbers for contours while excluding the specified region
+cell_numbers = print_cells_of_contours(contours, res, exclude_region)
 
-# Draw rectangles around detected yellow objects and display the result
+# Create an array to mark cells containing objects
+cells_with_objects = np.zeros((res, res), dtype=bool)
+
+# Mark the cells containing objects
+for cell_number in cell_numbers:
+    cell_x = cell_number % res
+    cell_y = cell_number // res
+    cells_with_objects[cell_y, cell_x] = True
+
+# Draw rectangles around cells containing objects
+cell_size = 500 // res
 result_image = img.copy()
-for i, contour in enumerate(contours):
-    x, y, w, h = cv2.boundingRect(contour)
-    cell_x, cell_y = get_cell(x + w // 2, y + h // 2, res)
-    if 0 <= cell_x < res and 0 <= cell_y < res and not (exclude_region[0] <= x < exclude_region[1] and exclude_region[2] <= y < exclude_region[3]):
-        cv2.rectangle(result_image, (x, y), (x + w, y + h), (0, 255, 0), 2)  # Draw green rectangles
+for cell_x in range(res):
+    for cell_y in range(res):
+        if cells_with_objects[cell_y, cell_x]:
+            x = cell_x * cell_size
+            y = cell_y * cell_size
+            cv2.rectangle(result_image, (x, y), (x + cell_size, y + cell_size), (0, 0, 255), 2)  # Draw red rectangles
 
-cv2.imshow("Yellow Objects", result_image)
+# Print the cell numbers containing objects
+for cell_number in cell_numbers:
+    print(f"Cell {cell_number} contains an object")
+
+cv2.imshow("Cells with Objects", result_image)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
 
